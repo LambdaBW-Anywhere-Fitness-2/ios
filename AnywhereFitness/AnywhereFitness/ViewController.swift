11c835d705af052cf1f34f9a48a43fe8ec762005
Josh Kocsis <josh.s.kocsis@gmail.com>
2020-06-17 21:41:35 -0500

    Inital Commit

---
+++ AnywhereFitness/AnywhereFitness/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnywhereFitness
+//
+//  Created by Josh Kocsis on 6/17/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
