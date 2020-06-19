2302574d27b31a6ca5ece84a8052cf3a1bc442a2
Josh Kocsis <josh.s.kocsis@gmail.com>
2020-06-19 00:06:25 -0500

    Inital setup of storyboard and controllers.

---
+++ AnywhereFitness/AnywhereFitness/Josh's Setup/View Controllers/HomePageViewController.swift
@@ -0,0 +1,32 @@
+//
+//  HomePageViewController.swift
+//  AnywhereFitness
+//
+//  Created by Josh Kocsis on 6/18/20.
+//  Copyright © 2020 Matthew Martindale. All rights reserved.
+//
+
+import UIKit
+
+class HomePageViewController: UIViewController {
+    @IBOutlet weak var homePageLabel: UILabel!
+    @IBOutlet weak var homePageWelcomeLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
