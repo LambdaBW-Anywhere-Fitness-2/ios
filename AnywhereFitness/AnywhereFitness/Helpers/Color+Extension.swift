f115065971d787e2662eef6d4d6133f581d3d720
Matt Martindale <martindalematt18@yahoo.com>
2020-06-18 19:35:36 -0600

    created UIColor helper file

---
+++ AnywhereFitness/AnywhereFitness/Helpers/Color+Extension.swift
@@ -0,0 +1,17 @@
+//
+//  Color+Extension.swift
+//  AnywhereFitness
+//
+//  Created by Matthew Martindale on 6/18/20.
+//  Copyright © 2020 Matthew Martindale. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static let fluorescentBlue: UIColor = #colorLiteral(red: 0.03921568627, green: 0.8941176471, blue: 0.9215686275, alpha: 1)
+    static let pacificBlue: UIColor = #colorLiteral(red: 0.2745098039, green: 0.6745098039, blue: 0.7607843137, alpha: 1)
+    static let darkCyan: UIColor = #colorLiteral(red: 0.2862745098, green: 0.5490196078, blue: 0.5411764706, alpha: 1)
+    static let kombuGreen: UIColor = #colorLiteral(red: 0.2, green: 0.2784313725, blue: 0.2352941176, alpha: 1)
+    static let smokyBlack: UIColor = #colorLiteral(red: 0.05098039216, green: 0.04705882353, blue: 0.03137254902, alpha: 1)
+}
