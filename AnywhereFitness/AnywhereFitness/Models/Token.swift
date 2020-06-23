353e0b01b1564b9cddd6f29f78d46518cc639a8c
Matt Martindale <martindalematt18@yahoo.com>
2020-06-22 19:51:18 -0600

    added Network Controller and Token Model

---
+++ AnywhereFitness/AnywhereFitness/Models/Token.swift
@@ -0,0 +1,13 @@
+//
+//  Token.swift
+//  AnywhereFitness
+//
+//  Created by Matthew Martindale on 6/22/20.
+//  Copyright © 2020 Matthew Martindale. All rights reserved.
+//
+
+import Foundation
+
+struct Token {
+    let token: String
+}
