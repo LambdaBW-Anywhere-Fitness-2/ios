7dc4f50a923c49861d34e11c2a9234194737affb
Josh Kocsis <josh.s.kocsis@gmail.com>
2020-06-22 21:53:59 -0500

    Added some folders and files for the class search storyboard

---
+++ AnywhereFitness/AnywhereFitness/Josh's Setup/Model/ClassSearch.swift
@@ -0,0 +1,13 @@
+//
+//  ClassSearch.swift
+//  AnywhereFitness
+//
+//  Created by Josh Kocsis on 6/22/20.
+//  Copyright © 2020 Matthew Martindale. All rights reserved.
+//
+
+import Foundation
+
+struct ClassSearch {
+    
+}
---
+++ AnywhereFitness/AnywhereFitness/Josh's Setup/Views/ClassSearchCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ClassSearchCollectionViewCell.swift
+//  AnywhereFitness
+//
+//  Created by Josh Kocsis on 6/22/20.
+//  Copyright © 2020 Matthew Martindale. All rights reserved.
+//
+
+import UIKit
+
+class ClassSearchCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var classSearchImage: UIImageView!
+    @IBOutlet weak var classSearchLabel: UILabel!
+
+    static let reuseIdentifier = "ClassSearchCell"
+
+}
