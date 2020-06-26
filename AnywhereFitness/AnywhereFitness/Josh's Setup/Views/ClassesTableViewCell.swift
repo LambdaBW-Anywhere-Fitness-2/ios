ba11d093d5eb713942ed65bc92fd4e2d10422c18
Josh Kocsis <josh.s.kocsis@gmail.com>
2020-06-25 22:38:18 -0500

    Added classes view controller and added some code to the defined search controller.

---
+++ AnywhereFitness/AnywhereFitness/Josh's Setup/Views/ClassesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ClassesTableViewCell.swift
+//  AnywhereFitness
+//
+//  Created by Josh Kocsis on 6/25/20.
+//  Copyright © 2020 Matthew Martindale. All rights reserved.
+//
+
+import UIKit
+
+class ClassesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
